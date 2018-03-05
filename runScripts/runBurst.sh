b2fd9a222c6cb4852ac2bf9e9accfd02a62756fe
John S <jsonch@seas.upenn.edu>
2018-03-05 08:57:19 -0500

    names

--- runScripts/runBurst.sh
+++
@@ -1,8 +0,0 @@
-. ./setDpdkPaths.bash
-./allocHugePages.sh
-WORK_DIR=`pwd`
-cd $PKTGEN_DIR
-# -l <list of cores to use>
-# -n <number of memory channels (always for for dell 720s)
-# -m [cores for rx:cores for tx].[device #s to use (0 for first NIC)]
-sudo ./app/x86_64-native-linuxapp-gcc/pktgen -l 0,1,2,3,4,5,6,7,8 -n 4 -w 08:00.1 -- -P -m "[1:2-8].[0]" -f $WORK_DIR/luaScripts/burst.lua
