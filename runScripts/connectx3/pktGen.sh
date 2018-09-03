b4e40dbd76f6dcc8d6a84d22ca9f9214c009acb3
jsonch <jsonch@seas.upenn.edu>
2018-09-03 17:03:20 -0400

    updated for tclust6 - 15

--- runScripts/connectx3/pktGen.sh
+++ runScripts/connectx3/pktGen.sh
@@ -4,4 +4,4 @@ cd $PKTGEN_DIR
 # -l <list of cores to use>
 # -n <number of memory channels (always for for dell 720s)
 # -m [cores for rx:cores for tx].[device #s to use (0 for first NIC)]
-sudo ./app/x86_64-native-linuxapp-gcc/pktgen -l 0,8,9,10,11 -n 2 -w 03:00.0 -w 03:00.1 -- -P -m "[8-9:10-11].[0-1]"
+sudo ./app/x86_64-native-linuxapp-gcc/pktgen -l 0,1,2,3,4,5,6,7 -n 2 -w 0d:00.0 -w 0d:00.1 -- -P -m "[1-6:7].[0-1]"
