5954723ebc1f36ad9b84fa2d009e980badeae6d8
John S <jsonch@seas.upenn.edu>
2018-09-19 11:12:18 -0400

    low util fg traffic

--- luaScripts/lowUtilBg.lua
+++ luaScripts/lowUtilBg.lua
@@ -24,7 +24,7 @@ pktgen.set_ipaddr(txPort, "dst", tgtIp)
 pktgen.set_ipaddr(txPort, "src", myIp)
 pktgen.set_mac(txPort, tgtMac)
 
-file = io.open("txBw.csv", "w");
+file = io.open("rxBw.csv", "w");
 
 print("STARTUP COMPLETE, sending packets.\n");
 
